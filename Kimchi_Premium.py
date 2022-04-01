c303ee87c83603c3db492419c1964e150f6993b2
beomsun0829 <ghsqjatns@naver.com>
2022-04-02 00:05:26 +0900

    complete

--- Kimchi_Premium.py
+++ Kimchi_Premium.py
@@ -70,13 +70,12 @@ while True :
     KRW_BTC = upbit.fetch_ticker('BTC/KRW')
     BTC_USDT = upbit.fetch_ticker('BTC/USDT')
     TetherPrice = KRW_BTC['last'] / BTC_USDT['last']
+    
     print("Tether Price : ", TetherPrice)
-    
-    
     for marketTicker in RefinedMarkets :
         if(1) :
             ticker = upbit.fetch_ticker(marketTicker +'/KRW')
-            resultDict[marketTicker] = {"Upbit_KRW" : ticker['close'] * 100/TetherPrice}
+            resultDict[marketTicker] = {"Upbit_KRW" : ticker['close'] / TetherPrice}
             time.sleep(0.03)
         
         if(RefinedMarkets[marketTicker][0] == 'Binance_USDT') :
@@ -85,7 +84,8 @@ while True :
                 resultDict[marketTicker]["Binance_USDT"] = ticker['close']
                 time.sleep(0.03)
             except :
-                print(marketTicker + "/USDT is not available")
+                #print(marketTicker + "/USDT is not available")
+                continue
         
         if(len(RefinedMarkets[marketTicker]) == 1):
             continue
@@ -96,23 +96,55 @@ while True :
                 resultDict[marketTicker]["Binance_BUSD"] = ticker['close']
                 time.sleep(0.03)
             except :
-                print(marketTicker + "/BUSD is not available")
-        
-    print(resultDict)
+                #print(marketTicker + "/BUSD is not available")
+                continue
             
     
     for ticker, valueList in resultDict.items() :
-        #get gap between Upbit_KRW and Binance_USDT
+        #search max value in valueList but not Upbit_KRW
+        maxValue = -10000
+        maxKey = ''
+        for key, value in valueList.items() :
+            if(key == 'Upbit_KRW') :
+                continue
+            if(value > maxValue) :
+                maxValue = value
+                maxKey = key
         
-        gap = valueList['Upbit_KRW'] / valueList['Binance_USDT']
-        print(ticker + " : " + str(gap))
+        #search min value in valueList but not Upbit_KRW
+        minValue = 10000
+        minKey = ''
+        for key, value in valueList.items() :
+            if(key == 'Upbit_KRW') :
+                continue
+            if(value < minValue) :
+                minValue = value
+                minKey = key
+        
+        try :
+            if(minValue > 0 and minValue != 10000) : 
+                gap = (valueList['Upbit_KRW'] - minValue) / minValue * 100
+                gapmarket = minKey
+                
+            elif(maxValue > 0 and maxValue != -10000) :
+                gap = (valueList['Upbit_KRW'] - maxValue) / maxValue * 100
+                gapmarket = maxKey
+                
+            else :
+                gap = 0
+    
+        except (ZeroDivisionError, KeyError) :
+            #print("ZeroDivisionError at" + ticker + "  " + str(valueList))
+            continue
+            
          
-        """
-        if maxVal > 1.05 * minVal and minVal != 0:
-            print(ticker, " 5% 이상 차이", "(",round((maxVal/minVal - 1)*100, 2), " %)", "| ", minMarket,"(", round(minVal, 2),")",  "| ", maxMarket, "(", round(maxVal, 2),")","\n")
-        elif maxVal > 1.03 * minVal and minVal != 0:
-            print(ticker, " 3% 이상 차이 ", "(",round((maxVal/minVal - 1)*100, 2), " %)", "| ", minMarket, "(",round(minVal, 2),")",  "| ", maxMarket, "(", round(maxVal, 2),")","\n")
-        """    
+        if abs(gap) > 5 :
+            print(ticker, " 5% 이상 차이", "(",round(gap, 3), " %)" + " | " + gapmarket)
+        elif abs(gap) > 3 :
+            print(ticker, " 3% 이상 차이 ", "(",round(gap, 3), " %)" + " | " + gapmarket)
+        elif abs(gap) > 1 :
+            print(ticker, " 1% 이상 차이 ", "(",round(gap, 3), " %)" + " | " + gapmarket)
+        
     print("시행 완료, 소요시간 (", round(time.time() - start, 1), "초)")
     now = time.localtime()
     print ("%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
